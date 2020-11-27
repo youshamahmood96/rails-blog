814f271383ef2ce412f1c2699a69eb972e726216
youshamahmood <youshamahmood96@gmail.com>
2020-11-27 07:19:47 +0600

    basic crud done

--- app/controllers/articles1_controller.rb
+++ app/controllers/articles1_controller.rb
@@ -5,6 +5,9 @@ class Articles1Controller < ApplicationController
     def index
         @articles = Articles1.all()
     end
+    def edit
+        @article = Articles1.find(params[:id])
+    end
     def new
         @article = Articles1.new
     end
@@ -16,5 +19,19 @@ class Articles1Controller < ApplicationController
        else
         render 'new'
        end 
+    end
+    def update
+        @article = Articles1.find(params[:id])
+        if @article.update(params.require(:articles1).permit(:title,:description))
+          flash[:notice] = "Article was successfully updated"
+          redirect_to @article
+        else
+          render 'edit'  
+        end
+    end
+    def destroy
+        @article = Articles1.find(params[:id])
+        @article.destroy
+        redirect_to articles1_index_path
     end
 end
