0706b6324c7d10d9a2bd7641b0a44026b6bb6b61
youshamahmood <youshamahmood96@gmail.com>
2020-11-29 00:16:09 +0600

    production

--- app/controllers/articles1_controller.rb
+++ app/controllers/articles1_controller.rb
@@ -1,18 +1,17 @@
 class Articles1Controller < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Articles1.find(params[:id])
     end
     def index
         @articles = Articles1.all()
     end
     def edit
-        @article = Articles1.find(params[:id])
     end
     def new
         @article = Articles1.new
     end
     def create
-       @article = Articles1.new(params.require(:article).permit(:title,:description))
+       @article = Articles1.new(white_list)
        if @article.save
         flash[:notice] = "Article was successfully created"
         redirect_to @article
@@ -21,8 +20,7 @@ class Articles1Controller < ApplicationController
        end 
     end
     def update
-        @article = Articles1.find(params[:id])
-        if @article.update(params.require(:articles1).permit(:title,:description))
+        if @article.update(white_list)
           flash[:notice] = "Article was successfully updated"
           redirect_to @article
         else
@@ -30,8 +28,14 @@ class Articles1Controller < ApplicationController
         end
     end
     def destroy
-        @article = Articles1.find(params[:id])
         @article.destroy
         redirect_to articles1_index_path
     end
+    private
+    def set_article
+        @article = Articles1.find(params[:id])
+    end
+    def white_list
+        params.require(:articles1).permit(:title,:description)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'about', to: 'pages#about'
   resources :articles1
 end
