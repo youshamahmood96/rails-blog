a23264a79eeb4ad65e4b1e2de83729c9c298b2ad
youshamahmood <youshamahmood96@gmail.com>
2020-11-30 05:40:18 +0600

    user-form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   resources :articles1
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
