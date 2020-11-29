56344e7083076192b219ae0a3369348cada77c32
youshamahmood <youshamahmood96@gmail.com>
2020-11-29 06:21:50 +0600

    created-user-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_144911) do
+ActiveRecord::Schema.define(version: 2020_11_29_001751) do
 
   create_table "articles", force: :cascade do |t|
-   
   end
 
   create_table "articles1s", force: :cascade do |t|
@@ -21,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_11_26_144911) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
