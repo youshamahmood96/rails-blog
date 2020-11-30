f6378934660fe12dda24c3401e3fa59c4167e7c5
youshamahmood <youshamahmood96@gmail.com>
2020-11-30 06:02:11 +0600

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] ="Welcome to the blog, #{@user.username} ! You have successfully signed up"
+            redirect_to articles1_index_path
+        else
+            render 'new'
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
