692ea2122ca75de77e12723755ef455ade7f1868
youshamahmood <youshamahmood96@gmail.com>
2020-11-30 07:05:38 +0600

    edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,11 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end
-    end  
+    end
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles1s 
+    end   
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def gravatar_for(user, options = {size:80})
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        gravatar_URL = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(gravatar_URL, alt:user.username, class: "mx-auto d-block")
+    end
 end
