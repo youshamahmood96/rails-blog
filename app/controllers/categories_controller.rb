dbefc5e4fe57f0ed4f12565355b6cf628c6f0b43
youshamahmood <youshamahmood96@gmail.com>
2020-12-01 06:14:43 +0600

    testing

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+    def index
+    end
+    def new
+        @category = Category.new
+    end
+    def show
+    end
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end     
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_055004) do
-
-  create_table "articles", force: :cascade do |t|
-  end
+ActiveRecord::Schema.define(version: 2020_11_30_232147) do
 
   create_table "articles1s", force: :cascade do |t|
     t.string "title"
@@ -23,13 +20,19 @@ ActiveRecord::Schema.define(version: 2020_11_30_055004) do
     t.integer "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "password_digest"
   end
 
 end
