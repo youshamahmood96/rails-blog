cdeba7a97634aef04871478e3cf54b5230c08e5c
youshamahmood <sirprocrastinatealot@gmail.com>
2020-12-01 10:05:27 +0600

    minor

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
         @category = Category.new(category_params)
         if @category.save
             flash[:notice] = "Category was successfully created"
-            redirect_to @category
+            redirect_to root_path
         else
             render 'new'
         end
